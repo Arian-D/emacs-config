99daaac56cfe51d142e3e439860864ee6037190b
Arian-D <ArianXDehghani@gmail.com>
2023-05-27 02:13:43 -0700

    Fix bar removal

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -3,8 +3,3 @@
       load-prefer-newer noninteractive
       default-input-method nil)
 (set-language-environment "UTF-8")
-
-(when (display-graphic-p)
-  (tool-bar-mode -1)
-  (menu-bar-mode -1)
-  (set-scroll-bar-mode nil))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
 ;;; -*- lexical-binding: t -*-
+
+(when (display-graphic-p)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (set-scroll-bar-mode nil))
+
 (let ((config-org-location (expand-file-name "config.org" user-emacs-directory)))
   (org-babel-load-file config-org-location))
