c0c7e6443ff6b32f3f46b4c8bd032c9f97171587
Arian <arianxdehghani@gmail.com>
2024-04-08 17:29:11 -0700

    Update flake; still a pain 😭

--- flake.nix
+++ flake.nix
@@ -1,24 +1,56 @@
 {
-  description = "My Emacs config";
+  description = "Arian's Emacs";
 
-  inputs = {
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils, emacs-overlay }:
-    with flake-utils.lib;
-    eachSystem [ system.x86_64-linux system.aarch64-linux ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ emacs-overlay.overlays.emacs ];
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+          emacs = pkgs.emacs29-pgtk;
+          dev-tools = with pkgs; [
+            git
+            bash
+            coreutils
+            findutils
+            cacert
+          ];
+          lsps = with pkgs; [
+            marksman
+            yaml-language-server
+            gopls
+            nil
+            rust-analyzer
+            terraform-ls
+          ];
+          all-the-packages = [ emacs ] ++ lsps ++ dev-tools;
+          env = pkgs.buildEnv {
+            name = "Emacs";
+            paths = all-the-packages;
+            pathsToLink = [ "/bin" ];
+          };
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "my-emacs";
+            tag = "latest";
+            created = "now";
+            copyToRoot = env;
+            runAsRoot = ''
+              #!${pkgs.runtimeShell}
+              mkdir -p /tmp
+            '';
+            config = {
+              Cmd = [ "${emacs}" ];
+            # TODO: Fix this to avoid having to specify the emacs path
+              Volumes = { "/data" = { }; };
+            };
+          };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = all-the-packages;
         };
-      in {
-        packages.default = (pkgs.emacsWithPackagesFromUsePackage {
-          config = ./.emacs.d/init.el;
-          defaultInitFile = true;
-        });
-      });
+        packages.environment = env;
+        packages.dockerImage = dockerImage;
+      }
+    );
 }
