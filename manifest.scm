109fddcecbecfa998e95714f076f58e840f55385
Arian <arianxdehghani@gmail.com>
2024-04-09 20:43:19 -0700

    Add basic guix manifest

---
+++ manifest.scm
@@ -0,0 +1,14 @@
+;; What follows is a "manifest" equivalent to the command line you gave.
+;; You can store it in a file that you may then pass to any 'guix' command
+;; that accepts a '--manifest' (or '-m') option.
+
+(specifications->manifest
+  (list "emacs-pgtk@29.3"
+        "git"
+        "bash"
+        "findutils"
+        "coreutils"
+        "font-fantasque-sans"
+        "font-latin-modern"
+        "unicode-emoji"
+        "fontconfig"))
