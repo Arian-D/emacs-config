387a42b6121262b9fc962e9b30073871bf6072ec
Arian <arianxdehghani@gmail.com>
2024-11-03 03:03:02 -0800

    Increase garbage collection threshold

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,5 +1,5 @@
 (setq package-enable-at-startup nil
-      gc-cons-threshold most-positive-fixnum
+      gc-cons-threshold (* 40 (expt 2 30))
       load-prefer-newer noninteractive
       default-input-method nil)
 (set-language-environment "UTF-8")
